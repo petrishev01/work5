660d5c5db4d34b84642b4ee30d36c796830136e5
Alexander <poltavetz.alexander@mail.ru>
2021-10-11 15:10:54 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+window.addEventListener("DOMContentLoaded", function (event) {console.log("DOM fully loaded and parsed");});
+
+function calculator() {
+    let number = Number(document.getElementById("number").value);
+    let cost = Number(document.getElementById("cost").value);
+
+
+    let re = new RegExp(/^[0-9]+$/i);
+    if (
+        !(
+            re.test(number) &&
+            re.test(cost)
+        )
+    ) {
+        alert("Нужно писать число!");
+    } else if (number == "") {
+        alert("Не указана цена товара");
+    } else if (cost == "") {
+        alert("Не указано количество товара");
+    } else {
+        const result = number * cost;
+        document.getElementById("result").innerHTML = "Итоговая стоимость: " + result + " рублей";
+    }
+}
